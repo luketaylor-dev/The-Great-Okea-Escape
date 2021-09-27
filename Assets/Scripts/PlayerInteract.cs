c65a2e8579f7db0ee266a24303fded495b736e7e
Luke Taylor <luke@dibza.co.uk>
2021-09-27 22:57:01 +0100

    move pickup logic to player interact script

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -1,53 +1,120 @@
 ﻿using System;
+using UnityEditor;
 using UnityEngine;
 
-namespace DefaultNamespace
+public class PlayerInteract : MonoBehaviour
 {
-    public class PlayerInteract : MonoBehaviour
+    public float interactDistance;
+    public float throwForce;
+    public Transform world;
+    public Transform destination;
+    public LayerMask playerMask;
+
+    private Camera mainCamera;
+    private GameObject holdingObject;
+
+    private void Start()
     {
-        public float interactDistance;
-        public Transform destination;
-        public LayerMask playerMask;
-        
-        private Camera mainCamera;
-        
-        private PickUp holdingObject;
-        
+        mainCamera = Camera.main;
+    }
 
-        private void Start()
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward) * 10);
+        if (holdingObject == null && Input.GetMouseButtonDown(0))
         {
-            mainCamera = Camera.main;
-        }
-
-        // Update is called once per frame
-        void Update(){
-            
-            Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward) * 10);
-            if (holdingObject == null && Input.GetMouseButtonDown(0))
+            Debug.Log("click");
+            if (Physics.Raycast(mainCamera.transform.position,
+                mainCamera.transform.TransformDirection(Vector3.forward), out var hit, interactDistance,
+                ~playerMask))
             {
-                Debug.Log("MouseDown");
-                if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward),out var hit, interactDistance,  ~playerMask))
+                Debug.Log(hit.distance);
+                if (hit.distance < interactDistance)
                 {
-                    Debug.Log(hit.transform.gameObject);
-                    if (hit.distance < interactDistance)
+                    if (hit.transform.gameObject.GetComponent<Rigidbody>() != null)
                     {
-                        holdingObject = hit.transform.gameObject.GetComponent<PickUp>();
-                        
-                        if (holdingObject != null)
-                        {
-                            holdingObject.GrabObject(destination);
-                        }
+                        Debug.Log(hit.transform.gameObject);
+                        GrabObject(hit.transform.gameObject);
                     }
                 }
             }
+        }
 
-            if (holdingObject != null && Input.GetMouseButtonDown(1))
+        if (holdingObject == null && Input.GetMouseButtonDown(1))
+        {
+            if (Physics.Raycast(mainCamera.transform.position,
+                mainCamera.transform.TransformDirection(Vector3.forward), out var hit, interactDistance,
+                ~playerMask))
             {
-                holdingObject.StopHold();
-                holdingObject.GetComponent<Rigidbody>().AddForce(mainCamera.transform.forward * holdingObject.power);
-                holdingObject = null;
+                if (hit.distance < interactDistance)
+                {
+                    ThrowObject(hit.transform.gameObject);
+                }
             }
+        }
+
+        if (holdingObject != null && Input.GetMouseButtonDown(1))
+        {
+            StopHoldAndThrow();
+        }
+
+        if (holdingObject != null && Input.GetKeyDown("e"))
+        {
+            StopHold();
         }
     }
 
+    private void GrabObject(GameObject gObject)
+    {
+        holdingObject = gObject;
+        if (holdingObject != null && holdingObject.GetComponent<Rigidbody>() != null)
+        {
+            //holdingObject.GetComponent<BoxCollider>().enabled = false;
+            holdingObject.GetComponent<Rigidbody>().useGravity = false;
+            holdingObject.GetComponent<Rigidbody>().freezeRotation = true;
+            holdingObject.GetComponent<Rigidbody>().isKinematic = true;
+            holdingObject.transform.position = destination.position;
+            holdingObject.transform.rotation = destination.rotation;
+            holdingObject.transform.parent = destination;
+        }
+    }
+
+    private void ThrowObject(GameObject gObject)
+    {
+        Debug.Log("throwCall");
+        if (gObject != null && gObject.GetComponent<Rigidbody>() != null)
+        {
+            Debug.Log("throwForce");
+            gObject.GetComponent<Rigidbody>().AddForce(mainCamera.transform.forward * throwForce);
+        }
+    }
+
+    private void StopHold()
+    {
+        if (holdingObject != null)
+        {
+            ResetObject();
+            holdingObject = null;
+        }
+    }
+
+    private void StopHoldAndThrow()
+    {
+        if (holdingObject != null)
+        {
+            ResetObject();
+            ThrowObject(holdingObject);
+            holdingObject = null;
+        }
+    }
+
+    private void ResetObject()
+    {
+        holdingObject.transform.parent = world;
+        //holdingObject.GetComponent<BoxCollider>().enabled = true;
+        holdingObject.GetComponent<Rigidbody>().useGravity = true;
+        holdingObject.GetComponent<Rigidbody>().freezeRotation = false;
+        holdingObject.GetComponent<Rigidbody>().isKinematic = false;
+    }
 }
